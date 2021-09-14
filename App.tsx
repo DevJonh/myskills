3309eeacd92a4c059c483aa4fc1e0b16f25c42ab
Jônatas Senna <53903172+DevJonh@users.noreply.github.com>
2021-09-14 09:09:51 -0300

    build: project creation

---
+++ App.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { StatusBar } from "react-native";
+import { Home } from "./src/pages/Home";
+
+const App = () => {
+  return (
+    <>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="#121015"
+        translucent={true}
+      />
+      <Home />
+    </>
+  );
+};
+
+export default App;
